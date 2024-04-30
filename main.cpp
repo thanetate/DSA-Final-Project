d77a8cc2604c04469f0dd0f323b55e389f068f74
Thane Tate <thanetate@my.unt.edu>
2024-04-30 15:39:49 -0500

    changes

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ void ReadandPopulateLinkedList(BinarySearchTree& StudentBST, LinkedList& Student
         // Add person to LinkedList
         StudentList.Addperson(id, name, dob, street, city, state, zip);
         // Add person to BST
-        StudentBST.insert(data);
+        //StudentBST.insert(data);
     }
 
     file.close();
@@ -84,118 +84,97 @@ int displaymenu()
     return choice;
 }
 
-int main(){
-//Linked List
-LinkedList StudentList;
-//BST
-BinarySearchTree StudentBST;
+int main() {
+    // Linked List
+    LinkedList StudentList;
+    // BST
+    BinarySearchTree StudentBST;
 
-//read and populate linked list with fake data
-ReadandPopulateLinkedList(StudentBST, StudentList, "fake_data.csv");
-//StudentList.Addperson(2021,"john", "01/01/2002", "Hickory","denton","TX",76201);
+    // read and populate linked list with fake data
+    ReadandPopulateLinkedList(StudentBST, StudentList, "fake_data.csv");
 
-int C;
-    do{//do while llop to ensure all functions are accesible and menu will pop up until usser exits program.
-    C = displaymenu();
-    if(C == 1){
-        //NodeData newStudent;
-        string n;
-        string dob, a,a2,a3;
-        int id, a4;
-        cout << "Enter name: ";
-        cin >> n;
-        //cin >> newStudent.id;
-        //getline(cin,n);
-        cout << "Enter ID: ";
-        cin >> id;
-        cout << "Enter Date of Birth: ";
-        cin.ignore();
-        getline(cin,dob);
-        //getline(cin, newStudent.name);
-        cout << "Address(Street): ";
-        cin >> a;
-        cout << "Address(City): ";
-        cin >> a2;
-        cout << "Address(State): ";
-        
-        cin>> a3;
-        cout << "Address(Zip Code): ";
-        
-        cin >> a4;
-        //Linked List
-        StudentList.Addperson(id,n,dob,a,a2,a3,a4);
-        //BST
-        //StudentBST.insert(newStudent);
-        }
-      else if (C ==2){
-        int id; 
-        cout << "Enter students ID: ";
-        cin >> id;
-        //LinkedList
-        StudentList.DeletePerson(id);
-        //BST
-        //StudentBST.deleteNodebyId(id);
-        break;
-    }
-    else if(C == 3){
-        int id;
-        cout << "Enter students ID: ";
-        cin >> id;
-        //LinkedList
-        Node* student = StudentList.SearchPerson(id);
-        if(student!= nullptr){
-            cout << "Student located: "<< student->name<<endl;
-        }
-        else{
-            cout << "Id provided is not found in our Student List"<<endl;
-        }
-        //BST
-        bool found = StudentBST.searchId(id);
-        if(found) {
-            cout<<"Student found in BST."<<endl;
-        } else {
-            cout<<"Student not found in BST."<<endl;
-        }
-        break;
-    }
-    else if(C== 4){
-        StudentList.DisplayEveryone();
-        break;
-    }
-    else if(C == 5){
-        
-        string n;
-        cout << "Enter student name: ";
-        cin >> n;
-        //LinkedList
-        Node* student = StudentList.SearchPersonN(n);
-        if(student!= nullptr){
-            cout << "Student located: "<< "name: "<< student->name<<endl;
-            cout << "ID: "<< student->ID << endl; 
-            cout << "DOB: "<< student->DOB<<endl;
-        }
-        else{
-            cout << "Name provided is not found in our Student List"<<endl;
-        }
-        //BST
-        bool found = StudentBST.searchName(n);
-        if (found) {
-                cout << "Student found in the BST." << endl;
-            } else {
-                cout << "Student not found in the BST." << endl;
+    int C;
+    do {
+        // do while loop to ensure all functions are accessible and menu will pop up until user exits program.
+        C = displaymenu();
+        switch (C) {
+            case 1: {
+                string n, dob, a, a2, a3;
+                int id, a4;
+                cout << "Enter name: ";
+                cin >> n;
+                cout << "Enter ID: ";
+                cin >> id;
+                cout << "Enter Date of Birth: ";
+                cin.ignore();
+                getline(cin, dob);
+                cout << "Address (Street): ";
+                cin >> a;
+                cout << "Address (City): ";
+                cin >> a2;
+                cout << "Address (State): ";
+                cin >> a3;
+                cout << "Address (Zip Code): ";
+                cin >> a4;
+                StudentList.Addperson(id, n, dob, a, a2, a3, a4);
+                // BST insertion could be uncommented if implemented
+                // StudentBST.insert(newStudent);
+                break;
             }
-        break;
-    }
-    else if(C == 6){
-        int n;
-        cout << "Enter ID: ";
-        cin >> n;
-        //Linked List
-        StudentList.UpdateInfo(n);
-    }
+            case 2: {
+                int id;
+                cout << "Enter student's ID: ";
+                cin >> id;
+                StudentList.DeletePerson(id);
+                // BST deletion could be uncommented if implemented
+                // StudentBST.deleteNodebyId(id);
+                break;
+            }
+            case 3: {
+                int id;
+                cout << "Enter student's ID: ";
+                cin >> id;
+                Node* student = StudentList.SearchPerson(id);
+                if (student != nullptr) {
+                    cout << "Student located: " << student->name << endl;
+                } else {
+                    cout << "ID provided is not found in our Student List" << endl;
+                }
+                // BST search could be included if implemented
+                // bool found = StudentBST.searchId(id);
+                break;
+            }
+            case 4:
+                StudentList.DisplayEveryone();
+                break;
+            case 5: {
+                string n;
+                cout << "Enter student name: ";
+                cin >> n;
+                Node* student = StudentList.SearchPersonN(n);
+                if (student != nullptr) {
+                    cout << "Student located: name: " << student->name << endl;
+                    cout << "ID: " << student->ID << endl;
+                    cout << "DOB: " << student->DOB << endl;
+                } else {
+                    cout << "Name provided is not found in our Student List" << endl;
+                }
+                break;
+            }
+            case 6: {
+                int n;
+                cout << "Enter ID: ";
+                cin >> n;
+                StudentList.UpdateInfo(n);
+                break;
+            }
+            case 7:
+                cout << "Exiting the program." << endl;
+                break; // This break exits the loop
+            default:
+                cout << "Invalid option. Please try again." << endl;
+        }
+    } while (C != 7);
 
-    }while (C != 7);
-
-    //end of program
     return 0;
 }

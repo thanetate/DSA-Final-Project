2b674115779047bb185dd3a0d777dffede731878
Oldsch0ol1 <69931258+Oldsch0ol1@users.noreply.github.com>
2024-04-30 12:05:26 -0500

    Updated the search and delete functions for ID only

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -15,7 +15,7 @@ TreeNode* BinarySearchTree::insertRec(TreeNode* root, const NodeData& data) {
     return root;
 }
 
-// search funtion for integers
+// search funtion for nodes ID
 bool BinarySearchTree::searchRecInt(TreeNode* root, int id) {
     if (root == nullptr) {
         return false;
@@ -30,52 +30,6 @@ bool BinarySearchTree::searchRecInt(TreeNode* root, int id) {
     }
 }
 
-// search function for strings
-bool BinarySearchTree::searchRecString(TreeNode* root, const string& name) {
-    if (root == nullptr) {
-        return false;
-    }
-
-    if (root->data.name == name) {
-        return true;
-    } else if (name < root->data.name) {
-        return searchRecString(root->left, name);
-    } else {
-        return searchRecString(root->right, name);
-    }
-}
-
-// function for deletion for ints and strings
-TreeNode* BinarySearchTree::deleteRecByName(TreeNode* root, const string& name) {
-    if (root == nullptr) {
-        return root;
-    }
-
-    // Recur down the tree
-    if (name < root->data.name) {
-        root->left = deleteRecByName(root->left, name);
-    } else if (name > root->data.name) {
-        root->right = deleteRecByName(root->right, name);
-    } else {
-        // Node with only one child/no child
-        if (root->left == nullptr) {
-            TreeNode* temp = root->right;
-            delete root;
-            return temp;
-        } else if (root->right == nullptr) {
-            TreeNode* temp = root->left;
-            delete root;
-            return temp;
-        }
-
-        //two children node
-        TreeNode* temp = minValueNode(root->right);
-        root->data = temp->data;
-        root->right = deleteRecByName(root->right, temp->data.name);
-    }
-    return root;
-}
-
 TreeNode* BinarySearchTree::minValueNode(TreeNode* node) {
     TreeNode* current = node;
 
@@ -86,13 +40,50 @@ TreeNode* BinarySearchTree::minValueNode(TreeNode* node) {
     return current;
 }
 
-// Public member functions
+// function for deletion for ints and strings
+TreeNode* deleteRecByID(TreeNode* root, int id) {
+    if (root == nullptr) {
+        return root;
+    }
 
-BinarySearchTree::BinarySearchTree() : root(nullptr) {}
+    // Recur down the tree
+    if (id < root->data.id) {
+        root->left = deleteRecByID(root->left, id);
+    } else if (id > root->data.id) {
+        root->right = deleteRecByID(root->right, id);
+    } else {
+        // Node with only one child or no child
+        if (root->left == nullptr) {
+            TreeNode* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            TreeNode* temp = root->left;
+            delete root;
+            return temp;
+        }
 
-BinarySearchTree::~BinarySearchTree() {
-    // Implement tree deletion here if needed
+        // Node with two children
+        TreeNode* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteRecByID(root->right, temp->data.id);
+    }
+    return root;
 }
+
+public:
+// Function to search for a node in the tree by ID
+bool searchByID(int id) {
+    return searchRecByID(root, id);
+}
+
+// Function to delete a node from the tree by ID
+void deleteNodeByID(int id) {
+    root = deleteRecByID(root, id);
+}
+    return root;
+}
+
 // Function to insert a node into the tree
 void BinarySearchTree::insert(const NodeData& data) {
     root = insertRec(root, data);
@@ -100,32 +91,4 @@ void BinarySearchTree::insert(const NodeData& data) {
 // search by ID
 bool BinarySearchTree::searchId(int id) {
     return searchRecInt(root, id);
-}
-// search by name
-bool BinarySearchTree::searchName(string name) {
-    return searchRecString(root, name);
-}
-// search by dob
-bool BinarySearchTree::searchDOB(string dob) {
-    return searchRecString(root, dob);
-}
-// search by street
-bool BinarySearchTree::searchStreet(string street) {
-    return searchRecString(root, street);
-}
-// search by city
-bool BinarySearchTree::searchCity(string city) {
-    return searchRecString(root, city);
-}
-// search by state
-bool BinarySearchTree::searchState(string state) {
-    return searchRecString(root, state);
-}
-// search by zip
-bool BinarySearchTree::search(int zip) {
-    return searchRecInt(root, zip);
-}
-// Function to delete a node from the tree by name of ints or strings
-void BinarySearchTree::deleteNodeByName(const string& name) {
-    root = deleteRecByName(root, name);
 }
